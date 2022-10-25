a8a840949fcfbd32feb095babd2e4e9292e016c7
Oly Rajak <olyrajak@gmail.com>
2022-10-25 23:00:47 +0530

    Mongoose.js Assignment 1 Complete

---
+++ block-BNaacn/code/model/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: String,
+    email: { type: String, lowercase: true },
+    age: { type: Number, default: 0 },
+    password: { type: String, minlength: 5 },
+    createdAt: { type: Date, default: new Date() },
+    favourites: [String],
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
